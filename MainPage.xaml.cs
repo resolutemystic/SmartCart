59eb71242d2b3e34e0cb658a2cc87009554841ef
Joshua-G-Sparks <jgsparks@gmx.com>
2025-04-07 01:26:02 -0500

    Revert "Initial commit of working SmartCart feature"
    This reverts commit 8b7d3ce3b214fd5107ed356e5564fb94b7abc82a.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,43 +1,25 @@
-﻿using Microsoft.Maui.Controls;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace SmartCart
+﻿namespace SmartCart
 {
     public partial class MainPage : ContentPage
     {
+        int count = 0;
+
         public MainPage()
         {
             InitializeComponent();
-            LoadGroceryItems(); // Load on startup
         }
 
-        private async void OnAddItemClicked(object sender, EventArgs e)
+        private void OnCounterClicked(object sender, EventArgs e)
         {
-            string itemName = ItemNameEntry.Text?.Trim();
+            count++;
 
-            if (string.IsNullOrEmpty(itemName))
-            {
-                StatusLabel.Text = "Please enter an item name.";
-                return;
-            }
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
 
-            await DatabaseService.AddItemAsync(itemName);
-
-            StatusLabel.Text = $"Item \"{itemName}\" saved to your list!";
-            ItemNameEntry.Text = string.Empty;
-
-            await LoadGroceryItems(); // Refresh list after adding
-        }
-
-        private async Task LoadGroceryItems()
-        {
-            List<GroceryItem> items = await DatabaseService.GetItemsAsync();
-            GroceryListView.ItemsSource = items;
+            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
+
 }
-
-
-
